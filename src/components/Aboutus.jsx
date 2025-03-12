8c9891ab78a9c2b4ad5a1212eff67647a4e4ef96
amirziyacode <amzdidi@gmail.com>
2025-03-12 14:48:15 +0330

    link all social media

--- src/components/Aboutus.jsx
+++ src/components/Aboutus.jsx
@@ -9,10 +9,11 @@ function Aboutus(){
                     <h2 style={{fontSize:"40px"}}>About Us</h2>
                     <p style={{letterSpacing:"1px",fontSize:"17px"}}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laboriosam, adipisci expedita ratione explicabo earum neque maxime et soluta dolorem, sapiente enim magnam quidem officiis. Facere illum voluptates officia sequi in?</p>
                     <div className="about-icons">
-                        <a href=""><FaInstagram className="instagram"/></a>
-                        <FaGithub className="github"/>
+                        <a href="https://www.instagram.com/amirziya.py?igsh=empvOWp6ZncwbHU5"><FaInstagram className="instagram"/></a>
+                        <a href="https://github.com/amirziyacode"><FaGithub className="github"/></a>
+                        <a href="https://www.linkedin.com/in/amir-ali-ziya-zadeh-19161b248?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"></a>
                         <FaLinkedin className="linkdin"/>
-                        <MdEmail className="email"/>
+                       <a href="mailto:amz.python@gmail.com"><MdEmail className="email"/></a> 
                     </div>
                 </div>
                 <img className="pic-about" src="src/assets/aboutus.jpg" alt="about-us" />
